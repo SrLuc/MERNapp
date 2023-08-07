23879571d4bbe8b2614a2555917f4bdbf01d6345
unknown <ruanlucenaramos@gmail.com>
2023-08-07 14:55:20 -0300

    feat: add repository logic to findALL metodth

--- src/repositories/djRepository.js
+++ src/repositories/djRepository.js
@@ -1,6 +1,6 @@
 import db from "../database/conection.js";
 
-//index repository function
+//index repository function and promisses
 export async function findAll() {
   const sql = "SELECT * FROM dj";
   return new Promise((resolve, reject) => {
