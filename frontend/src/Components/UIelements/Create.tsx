d19d7faf8817174fd85191be330aee023efe6307
unknown <ruanlucenaramos@gmail.com>
2023-08-22 16:33:17 -0300

    refacor: add handleSubmit function to add an Dj in Database

--- frontend/src/Components/UIelements/Create.tsx
+++ frontend/src/Components/UIelements/Create.tsx
@@ -1,11 +1,25 @@
+import axios from "axios";
 import * as S from "../Styles/ComponentStyles";
 import Header from "./Header";
 
 const Create = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const data = new FormData(e.currentTarget);
+    const id = data.get("id");
+    const nome = data.get("nome");
+    const vertente = data.get("vertente");
+    const body = {
+      id: id,
+      nome: nome,
+      vertente: vertente,
+    };
+    axios.post("http://localhost:4040/user", body);
+  };
+
   return (
     <S.StyledCreate>
       <Header title="Add New Dj" />
-      <form action="http://localhost:4040/user" method="POST">
+      <form onSubmit={handleSubmit}>
         <div>
           <label>ID</label>
           <input type="number" name="id" />
